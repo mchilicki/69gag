599072984b52552cbc55cf8273c790d2440a2ae1
Marcin Chilicki <marcin.chilicki96@gmail.com>
2020-01-24 20:22:47 +0100

    Add correct memes adjusting to page, not their actual size

--- frontend/69gag/src/app/app.routing.ts
+++ frontend/69gag/src/app/app.routing.ts
@@ -8,7 +8,7 @@ import { MemeDetailsComponent } from './meme-details';
 import { AddMemeComponent } from './add-meme/add-meme.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'meme/:id', component: MemeDetailsComponent },
